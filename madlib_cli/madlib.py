127c027021e2198760b38eb7dbce47c8c3328d60
Sewar-web <s.maqableh@yahoo.com>
2021-06-19 17:23:02 +0300

    done

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -6,8 +6,7 @@ print("""
      This is a phrasal template word game 
      where one player prompts others for  
      a list of words to substitute for    
-     blanks in a story before reading     
-      aloud.                              
+     blanks in a story before reading it.                            
     ****************************************
     """)
 
