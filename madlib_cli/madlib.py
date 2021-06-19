d0cb0cb1be341db2d64738137c4fd6d39e36e0c2
Sewar-web <s.maqableh@yahoo.com>
2021-06-19 17:21:15 +0300

    done

--- madlib_cli/madlib.py
+++ madlib_cli/madlib.py
@@ -1,5 +1,15 @@
 import re
 
+print("""
+    ****************************************
+             Welcome to madlib            
+     This is a phrasal template word game 
+     where one player prompts others for  
+     a list of words to substitute for    
+     blanks in a story before reading     
+      aloud.                              
+    ****************************************
+    """)
 
 def read_template(path):
     try:
@@ -10,7 +20,7 @@ def read_template(path):
     except Exception as e:
         return "Something's Going Wrong : "+ e
 
-
+ 
 
 def parse_template(text):
     parse= re.findall(r'\{(.*?)\}', text)
